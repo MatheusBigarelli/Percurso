b6b70f2c11be50d32934ee4215595195bd0dda86
seffiroth <perva159@gmail.com>
2017-10-09 22:07:26 -0300

    Final?

--- src/main/java/utfpr/dainf/ct/ed/exemplo/ArvoreBinaria.java
+++ src/main/java/utfpr/dainf/ct/ed/exemplo/ArvoreBinaria.java
@@ -1,5 +1,6 @@
 package utfpr.dainf.ct.ed.exemplo;
-
+package arvorebinaria;
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -88,6 +89,160 @@ public class ArvoreBinaria<E> {
     public void visitaEmOrdem() {
         visitaEmOrdem(this);
     }
+    
+    
+    //----------------------------------------------------------------------------------------------------------------------------------
+
+    // Visita pre ordem recursivo
+    public void visitaPreOrdem()
+    {
+        visitaPreOrdem(this);
+    }
+
+    public void visitaPreOrdem(ArvoreBinaria<E> a)
+    {
+        visita(a);
+        if (a != null)
+        {
+            if (a.esquerda != null)
+                visitaPreOrdem(a.esquerda);
+
+            if (a.direita != null)
+                visitaPreOrdem(a.direita);
+        }
+    }
+
+
+    // Visita pre ordem iterativo
+    public ArvoreBinaria<E> proximoPreOrdem()
+    {
+        ArvoreBinaria<E> resultado;
+        resultado = null;
+     
+        if (inicio)
+        {
+            reinicia();
+            pilha.push(this);
+            inicio = false;
+        }
+        
+        if (!pilha.isEmpty())
+        {
+            resultado = pilha.pop();
+            
+            if (resultado.direita != null)
+                pilha.push(resultado.direita);
+            if (resultado.esquerda != null)
+                pilha.push(resultado.esquerda);
+        }
+
+        if (resultado == null)
+        {
+            inicio = true;
+            tiraVisitado(this);
+        }
+
+        return resultado;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------------------
+    // Visita pos ordem recursivo
+    public void visitaPosOrdem() { visitaPosOrdem(this); }
+
+    public void visitaPosOrdem(ArvoreBinaria<E> no)
+    {
+        if (no.esquerda != null)
+            visitaPosOrdem(no.esquerda);
+        if (no.direita != null)
+            visitaPosOrdem(no.direita);
+        visita(no);
+    }
+
+    // Visita pos ordem iterativo
+    public ArvoreBinaria<E> proximoPosOrdem()
+    {
+        ArvoreBinaria<E> resultado, pai = null;
+        resultado = this;
+
+        while (resultado != null)
+        {
+            if (resultado.visitado == false)
+            {
+                pai = resultado;
+                resultado = resultado.esquerda;
+                if (resultado == null && pai != null)
+                    resultado = pai.direita;
+            }
+            else
+            {
+                if (pai != null)
+                   resultado = pai.direita;   
+                if (resultado != null && resultado.visitado == true)
+                    break;
+            }
+        }
+
+        resultado = pai;
+        
+        if (resultado == null)
+        {
+            inicio = true;
+            tiraVisitado(this);
+        }
+
+        else
+            resultado.visitado = true;
+
+        return resultado;
+    }
+
+
+    private void tiraVisitado(ArvoreBinaria<E> a)
+    {
+        if (a != null)
+        {
+            a.visitado = false;
+            tiraVisitado(a.esquerda);
+            tiraVisitado(a.direita);
+        }
+    }
+
+
+//----------------------------------------------------------------------------------------------------------------------------------
+
+    //Visita em nível iterativo
+    public ArvoreBinaria<E> proximoEmNivel()
+    {
+        ArvoreBinaria<E> resultado;
+        if (fila == null)
+            fila = new LinkedList<>();
+        if (inicio)
+        {
+            inicio = false;
+            fila.add(this);
+        }
+
+
+        resultado = fila.pollFirst();
+        
+        if (resultado != null)
+        {                
+            if (resultado.esquerda != null)
+                fila.add(resultado.esquerda);
+            if (resultado.direita != null)
+                fila.add(resultado.direita);
+        }
+
+
+        if (resultado == null)
+        {
+            tiraVisitado(this);
+            inicio = true;
+        }
+
+        return resultado;
+    } 
+
     
     private void inicializaPilha() {
         if (pilha == null) {
